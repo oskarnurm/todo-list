1814ff14ffd4c7a0e1238c8c78b2665b27622265
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-08-24 12:55:32 +0200

    chore(config): split webpack configs

---
+++ webpack.prod.js
@@ -0,0 +1,28 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      `...`, // Keep default minimizers
+      new CssMinimizerPlugin(),
+    ],
+  },
+});
