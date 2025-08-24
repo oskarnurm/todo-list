3498aaec71106efd1e864ae24d12e1858696df10
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-08-24 22:28:56 +0200

    feat(index): import modules and test them

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+import "./style.css";
+import Project from "./Projects";
+import Task from "./Task";
+
+const p = new Project("school");
+const t1 = new Task("Homework");
+
+p.add(t1);
+console.log(p.getLib());
